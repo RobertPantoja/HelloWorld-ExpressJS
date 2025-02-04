ccc92b3e3a1af3bbfc567b4b9ed7bc1abface93d
Roberto Pantoja <rpantoja@polyworksmexico.com>
2025-02-04 16:36:27 -0600

    Added a endpoint

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.listen(port, () => {
+    console.log(`It´s alive on port ${port}, http://localhost:${port}`)
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World')
-})
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.get('/endpoint', (req, res) => {
+    res.status(200).send({
+        response: 'Test',
+        passed: 'Yes ✔'
+    })
+
+});
